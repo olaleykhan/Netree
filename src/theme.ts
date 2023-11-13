e4df2686c96be7860648456928e5f48166aa610d
lekan.dev <realjoy9@gmail.com>
2023-11-13 16:15:01 +0100

    add typography themeing

--- src/theme.ts
+++ src/theme.ts
@@ -12,7 +12,6 @@ const whiteColor = '#FAFAFA';
 const blackColor = '#333333';
 const errorColor = '#FF3939';
 
-const spacingUnit = 8;
 const spacing = (factor: number) => `${factor * 0.8}rem`;
 
 const theme = createTheme({
@@ -26,7 +25,9 @@ const theme = createTheme({
       main: secondaryColor,
     },
     background: {
-      default: backgroundColor,
+      // TODO: use more appropriate backgrpound to make app look better
+      default: whiteColor,
+      paper: backgroundColor,
     },
     text: {
       primary: textColor,
@@ -44,17 +45,57 @@ const theme = createTheme({
     }
   },
   typography: {
-    fontFamily: 'Roboto, sans-serif',
-    h1: {
+    fontFamily: 'Instrument Sans, Roboto, sans-serif',
+    h1  : {
       fontSize: '2rem',
       fontWeight: 600,
       color: blackColor,
     },
     h2: {
-      fontSize: '1.8rem',
-      fontWeight: 500,
+      fontSize: '3.2rem',
+      fontWeight: 700,
       color: blackColor,
+      lineHeight: '150%',
+      '@media (max-width:600px)': {
+        fontSize: '2.4rem',
+      },
     },
+    h3:{
+      fontSize: '1.6rem',
+      fontWeight: 600,
+      lineHeight: '150%',
+      fontStyle: 'normal',
+      color: blackColor,
+      '@media (max-width:600px)': {
+        fontSize: '1.2rem',
+      },
+
+    },
+    h4: undefined,
+    h5: undefined,
+    h6: undefined,
+    body1:{
+      fontSize: '1.6rem',
+      fontWeight: 400,
+      lineHeight: '150%',
+      fontStyle: 'normal',
+      color: blackColor,
+      '@media (max-width:600px)': {
+        fontSize: '1.2rem',
+      },
+    },
+    body2:{
+      fontSize: '1.2rem',
+      fontWeight: 400, 
+      lineHeight: '150%',
+      fontStyle: 'normal',
+      color: blackColor,
+      '@media (max-width:600px)': {
+        fontSize: '1rem',
+      },
+      subtitle1:undefined,
+      subtitle2:undefined,
+    }
     // Add more typography styles as needed
   },
   components: {
