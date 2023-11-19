00a5ed173bdfe2505d560189a444ebf48efe55fc
lekan.dev <realjoy9@gmail.com>
2023-11-19 05:46:30 +0100

    link select constants

---
+++ src/pages/home/constants.ts
@@ -0,0 +1,87 @@
+import codepenIcon from "@/assets/images/icon-codepen.svg"
+import facebookIcon from "@/assets/images/icon-facebook.svg"
+import githubIcon from "@/assets/images/icon-github.svg"
+import linkedinIcon from "@/assets/images/icon-linkedin.svg"
+import twitterIcon from "@/assets/images/icon-twitter.svg"
+import youtubeIcon from "@/assets/images/icon-youtube.svg"
+import gitlabIcon from "@/assets/images/icon-gitlab.svg"
+import codeWarsIcons from "@/assets/images/icon-codewars.svg"
+import devtoIcon from "@/assets/images/icon-devto.svg"
+import emailIcon from "@/assets/images/icon-email.svg"
+import freecodecampIcon from "@/assets/images/icon-freecodecamp.svg"
+import frontendmentorIcon from "@/assets/images/icon-frontend-mentor.svg"
+import hashnodeIcon from "@/assets/images/icon-hashnode.svg"
+
+export const LINK_OPTIONS: { value: string; label: string, icon: string }[]  = [
+    {
+        value: "github",
+        label: "GitHub",
+        icon: githubIcon
+    },
+    {
+        value: "linkedin",
+        label: "LinkedIn",
+        icon: linkedinIcon
+    },
+    {
+        value: "twitter",
+        label: "Twitter",
+        icon: twitterIcon
+    },
+    // {
+    //     value: "instagram",
+    //     label: "Instagram",
+    //     icon: instagramIcon
+    // },
+    {
+        value: "facebook",
+        label: "Facebook",
+        icon: facebookIcon
+    },
+    {
+        value: "youtube",
+        label: "YouTube",
+        icon: youtubeIcon
+    },
+    {
+        value: "codepen",
+        label: "Codepen",
+        icon: codepenIcon
+    },{
+        value: "gitlab",
+        label: "GitLab",
+        icon: gitlabIcon
+    },
+    {
+        value: "devto",
+        label: "Dev.to",
+        icon: devtoIcon
+    },
+    {
+        value: "hashnode",
+        label: "Hashnode",
+        icon: hashnodeIcon
+    },
+    {
+        value: "freecodecamp",
+        label: "FreeCodeCamp",
+        icon: freecodecampIcon
+    },
+    {
+        value: "frontendmentor",
+        label: "Frontend Mentor",
+        icon: frontendmentorIcon
+    },
+    {
+        value: "email",
+        label: "Email",
+        icon: emailIcon
+    },
+    {
+        value: "codewars",
+        label: "CodeWars",
+        icon: codeWarsIcons
+    }
+
+
+]
