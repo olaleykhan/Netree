4c2c00dd78614400797333a03e855f635a3e013c
olaleykhan <realjoy9@gmail.com>
2024-01-31 11:05:17 +0100

    clean

--- server/src/routes/users/users.router.ts
+++ server/src/routes/users/users.router.ts
@@ -1,18 +1,11 @@
 import express from 'express';
 
 import { httpGetAllUsers, httpCreateUser, httpGetUserById } from './users.controller.js';
-// import { httpUserSignup, httpUserLogin, httpProtect } from './auth.controller.js';
 
 const router = express.Router();
-
-// router.post('/signup', httpUserSignup);
-// router.post('/login', httpUserLogin);
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.get('/', httpGetAllUsers);
 
 router.get('/:id', httpGetUserById);
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.post('/', httpCreateUser);
 
 export default router;
